9e43c02aeec8a50a638545d28b2fe805b7e51370
DIR\guray.cintir <guray.cintir@accenture.com>
2024-02-21 23:54:42 +0300

    Implement business logic for lambda function | Add unit tests

--- function/input-text-reverser-lambda/src/main/java/com/awscloudprojects/lambda/InputTextReverserLambda.java
+++ function/input-text-reverser-lambda/src/main/java/com/awscloudprojects/lambda/InputTextReverserLambda.java
@@ -2,15 +2,36 @@ package com.awscloudprojects.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.awscloudprojects.model.InputText;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.lambda.powertools.logging.Logging;
 
 public class InputTextReverserLambda implements RequestHandler<Object, String> {
 
     private static final Logger logger = LoggerFactory.getLogger(InputTextReverserLambda.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     @Override
+    @Logging(logEvent = true, clearState = true)
     public String handleRequest(Object input, Context context) {
-        return null;
+        String response;
+        try {
+            if (Objects.isNull(input)) {
+                return "Input event is null";
+            }
+            InputText inputText = objectMapper.readValue((String) input, InputText.class);
+            if (Objects.isNull(inputText.getText())) {
+                return "Input text is null";
+            }
+            response = new StringBuilder(inputText.getText()).reverse().toString();
+            logger.info("received text: {} response: {}", inputText.getText(), response);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            response = e.getMessage();
+        }
+        return response;
     }
 }
 
--- function/input-text-reverser-lambda/src/test/java/com/awscloudprojects/InputTextReverserLambdaTest.java
+++ function/input-text-reverser-lambda/src/test/java/com/awscloudprojects/InputTextReverserLambdaTest.java
@@ -1,8 +1,12 @@
 package com.awscloudprojects;
 
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.awscloudprojects.lambda.InputTextReverserLambda;
+import com.awscloudprojects.model.InputText;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -10,14 +14,43 @@ import org.slf4j.LoggerFactory;
 
 public class InputTextReverserLambdaTest {
     private static final Logger logger = LoggerFactory.getLogger(InputTextReverserLambdaTest.class);
-
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     @Test
-    public void shouldReturnNull()
-    {
-        logger.info("Running test");
-        var event = new HashMap<String,String>();
+    public void shouldReturnReversedTextWhenInputTextValid() throws JsonProcessingException {
+        InputText inputText = new InputText();
+        inputText.setText("hello-world");
         Context context = new TestContext();
         InputTextReverserLambda inputTextReverserLambda = new InputTextReverserLambda();
-        assertNull(inputTextReverserLambda.handleRequest(event, context));
+        String response = inputTextReverserLambda.handleRequest(objectMapper.writeValueAsString(inputText), context);
+        assertNotNull(response);
+        assertEquals("dlrow-olleh", response);
+    }
+    @Test
+    public void shouldReturnResponseWhenInputEventNull() {
+        Context context = new TestContext();
+        InputTextReverserLambda inputTextReverserLambda = new InputTextReverserLambda();
+        String response = inputTextReverserLambda.handleRequest(null, context);
+        assertNotNull(response);
+        assertEquals("Input event is null", response);
+    }
+    @Test
+    public void shouldReturnResponseWhenInputTextNull() throws JsonProcessingException {
+        InputText inputText = new InputText();
+        inputText.setText(null);
+        Context context = new TestContext();
+        InputTextReverserLambda inputTextReverserLambda = new InputTextReverserLambda();
+        String response = inputTextReverserLambda.handleRequest(objectMapper.writeValueAsString(inputText), context);
+        assertNotNull(response);
+        assertEquals("Input text is null", response);
+    }
+
+    @Test
+    public void shouldReturnResponseWhenInputEventMalformed() {
+        var event = new HashMap<String, Object>();
+        event.put("text","test");
+        Context context = new TestContext();
+        InputTextReverserLambda inputTextReverserLambda = new InputTextReverserLambda();
+        String response = inputTextReverserLambda.handleRequest(event, context);
+        assertNotNull(response);
     }
 }
